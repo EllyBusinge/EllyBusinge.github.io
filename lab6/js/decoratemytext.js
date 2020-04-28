09002c776b8e16849bfe87f3cea449a93de33169
EllyB'ge <ell86bu@gmail.com>
2020-04-27 21:13:32 -0500

    Lab 6 solutions

--- lab6/js/decoratemytext.js
+++ lab6/js/decoratemytext.js
@@ -1,7 +1,9 @@
+"use strict;"
 
-function displayAlert(){
+function setTxtFont(){
     let textAreaBox = document.getElementById("txt1");
-    textAreaBox.className = "biggerDeco";
+    //textAreaBox.className = "biggerDeco";
+    textAreaBox.style.fontSize = getCurrentFontSize() + 2 + "pt";
 }
 
 function displayBling(){
@@ -12,18 +14,25 @@ function displayBling(){
         textAreaBox.style.fontWeight = "bold";
         textAreaBox.style.color = "green";
         textAreaBox.style.textDecoration = "underline";
+        document.body.style.backgroundImage = "url(images/hundred-dollar-bill.jpg)";
     } else {
         textAreaBox.style.fontWeight = "normal";
         textAreaBox.style.color = "black";
         textAreaBox.style.textDecoration = "none";
+        document.body.style.backgroundImage = "";
     }
 }
 
-window.onload = function() {
-    "use strict;"
+function getCurrentFontSize() {
+    let fontSize = window.getComputedStyle(document
+        .getElementById("txt1"), null)
+        .getPropertyValue('font-size');
+    return parseInt(fontSize);
+}
 
+window.onload = function() {
     let button = document.getElementById("biggerDeco");
-    button.onclick = displayAlert;
+    button.onclick = setTxtFont;
 
     let checkbox = document.getElementById("chkbx");
     checkbox.onclick = displayBling;
