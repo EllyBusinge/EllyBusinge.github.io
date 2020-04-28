7ba1d8a42c05c6721767b30c668f8e8170802bb3
EllyB'ge <ell86bu@gmail.com>
2020-04-27 20:19:48 -0500

    Lab 6 solutions

--- lab6/js/decoratemytext.js
+++ lab6/js/decoratemytext.js
@@ -1,10 +1,30 @@
 
 function displayAlert(){
-    var textAreaBox = document.getElementById("txt1");
+    let textAreaBox = document.getElementById("txt1");
     textAreaBox.className = "biggerDeco";
 }
 
+function displayBling(){
+    let checkbox = document.getElementById("chkbx");
+    let textAreaBox = document.getElementById("txt1");
+
+    if (checkbox.checked) {
+        textAreaBox.style.fontWeight = "bold";
+        textAreaBox.style.color = "green";
+        textAreaBox.style.textDecoration = "underline";
+    } else {
+        textAreaBox.style.fontWeight = "normal";
+        textAreaBox.style.color = "black";
+        textAreaBox.style.textDecoration = "none";
+    }
+}
+
 window.onload = function() {
-    var button = document.getElementById("biggerDeco");
+    "use strict;"
+
+    let button = document.getElementById("biggerDeco");
     button.onclick = displayAlert;
+
+    let checkbox = document.getElementById("chkbx");
+    checkbox.onclick = displayBling;
 }
