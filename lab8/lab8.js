3c1a7c5407cce3d4de828ceadfdc794a093de2ce
EllyB'ge <ell86bu@gmail.com>
2020-04-29 22:20:50 -0500

    Lab 8 solutions

--- lab8/lab8.js
+++ lab8/lab8.js
@@ -35,7 +35,102 @@ function makeAdder(inc) {
     }
 };
 
-let add5 = makeAdder(5);
-console.log(add5());
-console.log(add5());
-console.log(add5());
+// let add5 = makeAdder(5);
+// console.log(add5());
+// console.log(add5());
+// console.log(add5());
+
+/**
+ * Solution to number 9
+ */
+
+ /**
+ * Solution to number 10
+ */
+const Employee = (function() {
+    let name;
+    let age;
+    let salary;
+
+    const setName = function (newName) {
+        name = newName;
+    };
+
+    const setAge = function (newAge) {
+        age = newAge;
+    };
+
+    const setSalary = function (newSalary) {
+        salary = newSalary;
+    };
+
+    const getName = function () {
+        return name;
+    };
+
+    const getAge = function () {
+        return age;
+    };
+
+    const getSalary = function () {
+        return salary;
+    };
+
+    const incrementAge = function (increment) {
+        age = getAge() + increment;
+    };
+
+    const increaseSalary = function (percentage) {
+        salary = getSalary() * (1 + percentage/100);
+    };
+
+    const toString = function() {
+        return `Name: ${name}, Salary: ${salary}, Age: ${age}`;
+    }
+
+    return {
+        setName: setName,
+        setAge: setAge,
+        setSalary: setSalary,
+        incrementAge: incrementAge,
+        increaseSalary: increaseSalary,
+        toString: toString
+    };
+})();
+
+// Employee.setName("Elly");
+// Employee.setAge(40);
+// Employee.setSalary(600000);
+// console.log(Employee.toString());
+// Employee.increaseSalary(1000);
+// Employee.incrementAge(3);
+// console.log(Employee.toString());
+
+ /**
+ * Solution to number 11
+ */
+
+const EmployeeWithAddress = (function() {
+    let empAddress;
+    
+    const setEmpAddress = function (address) {
+        empAddress = address;
+    };
+
+    const getEmpAddress = function () {
+        return empAddress;
+    };
+
+    return {
+        setEmpAddress: setEmpAddress,
+        getEmpAddress: getEmpAddress,
+        __proto__: Employee
+    };
+})();
+
+EmployeeWithAddress.setName("Elly");
+EmployeeWithAddress.setAge(40);
+EmployeeWithAddress.setSalary(600000);
+EmployeeWithAddress.setEmpAddress("Fairfield, Iowa #88");
+console.log(EmployeeWithAddress.getEmpAddress());
+//console.log(Employee.address);
